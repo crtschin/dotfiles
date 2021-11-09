eab275d45cdae526f4e39f67f0e111054cbd8ed2
Curtis Chin Jen Sem <curtis.chinjensem@channable.com>
2021-11-09 16:34:30 +0100

    Update

--- work.nix
+++ work.nix
@@ -49,27 +49,26 @@
     };
 
     fish = {
-      interactiveShellInit =
-        ''
-begin
-  set fish_greeting
-  set __done_notify_sound 1
-  set LESS ' -R '
+      interactiveShellInit = ''
+        begin
+          set fish_greeting
+          set __done_notify_sound 1
+          set LESS ' -R '
 
-  # Non-NixOS setting
-  set --export NIX_PATH $NIX_PATH:$HOME/.nix-defexpr/channels
+          # Non-NixOS setting
+          set --export NIX_PATH $NIX_PATH:$HOME/.nix-defexpr/channels
 
-  # Channable specific
-  . ${.config/channable.fish}
-end
+          # Channable specific
+          . ${.config/channable.fish}
+        end
 
-alias l "exa"
-alias ll "exa -la"
-alias ls "exa"
-alias less "bat"
-alias g "git"
-alias e "eval $EDITOR"
-alias ee "e (fzf)"
+        alias l "exa"
+        alias ll "exa -la"
+        alias ls "exa"
+        alias less "bat"
+        alias g "git"
+        alias e "eval $EDITOR"
+        alias ee "e (fzf)"
         ''
       ;
     };
