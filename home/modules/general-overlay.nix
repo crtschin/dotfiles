14d046155da71fbf35b0dce78c9c8559658e22ca
crtschin <csochinjensem@gmail.com>
2024-12-23 19:29:40 +0100

    Update

--- home/modules/general-overlay.nix
+++ home/modules/general-overlay.nix
@@ -6,6 +6,27 @@
 }:
 let
   overlay = self: super: {
+    # Usage notice, install wrapped packages in `/usr/bin`
+    native_wrapper =
+      program:
+      pkgs.writeShellScriptBin program.pname ''
+        PATH=/usr/bin:${program}/bin ${program.pname} "$@"
+      '';
+
+    monitor-heap = pkgs.writeShellScriptBin "monitor-heap" ''
+      #!/bin/sh
+      head -`fgrep -n END_SAMPLE $1.hp | tail -1 | cut -d : -f 1` $1.hp \
+        | hp2ps > $1.ps
+      gv $1.ps &
+      gvpsnum=$!
+      while [ 1 ] ; do
+        sleep 10
+        head -`fgrep -n END_SAMPLE $1.hp | tail -1 | cut -d : -f 1` $1.hp \
+          | hp2ps > $1.ps
+        kill -HUP $gvpsnum
+      done
+    '';
+
     # https://github.com/NixOS/nixpkgs/issues/292700https://github.com/NixOS/nixpkgs/issues/292700
     flameshotGrim = pkgs.flameshot.overrideAttrs (oldAttrs: {
       src = pkgs.fetchFromGitHub {
--- work.nix
+++ work.nix
@@ -6,27 +6,6 @@
 }:
 let
   overlay = self: super: {
-    # Usage notice, install wrapped packages in `/usr/bin`
-    native_wrapper =
-      program:
-      pkgs.writeShellScriptBin program.pname ''
-        PATH=/usr/bin:${program}/bin ${program.pname} "$@"
-      '';
-
-    monitor-heap = pkgs.writeShellScriptBin "monitor-heap" ''
-      #!/bin/sh
-      head -`fgrep -n END_SAMPLE $1.hp | tail -1 | cut -d : -f 1` $1.hp \
-        | hp2ps > $1.ps
-      gv $1.ps &
-      gvpsnum=$!
-      while [ 1 ] ; do
-        sleep 10
-        head -`fgrep -n END_SAMPLE $1.hp | tail -1 | cut -d : -f 1` $1.hp \
-          | hp2ps > $1.ps
-        kill -HUP $gvpsnum
-      done
-    '';
-
     useWayland = true;
     # betterlockscreen = self.native_wrapper super.betterlockscreen;
     # i3lock-color = self.native_wrapper super.i3lock-color;
