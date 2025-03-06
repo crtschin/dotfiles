49b31809746122d927d205e4ab8e5b47896af6d6
crtschin <csochinjensem@gmail.com>
2025-03-06 22:18:02 +0100

    Update

--- home/modules/kanshi.nix
+++ home/modules/kanshi.nix
@@ -98,6 +98,30 @@ in
               }
             ];
           }
+          (createDualDockedIdenticalProfile {
+            left_criteria = "AOC 2460G5 F54GABA004914";
+            right_criteria = "Samsung Electric Company C27HG7x HTHJ700995";
+            width = 1920;
+            height = 1080;
+          })
+          (createDualDockedIdenticalProfile {
+            left_criteria = "AOC 2460G5 0x00001332";
+            right_criteria = "Samsung Electric Company C27HG7x HTHJ700995";
+            width = 1920;
+            height = 1080;
+          })
+          (createDualDockedIdenticalProfile {
+            left_criteria = "Dell Inc. DELL U2719D 74RRT13";
+            right_criteria = "Dell Inc. DELL U2717D J0XYN99BA3TS";
+            width = 2560;
+            height = 1440;
+          })
+          (createDualDockedIdenticalProfile {
+            left_criteria = "Dell Inc. DELL U2717D J0XYN989CU9S";
+            right_criteria = "Dell Inc. DELL U2719D 7RRBC23";
+            width = 2560;
+            height = 1440;
+          })
           {
             profile.name = "home";
             profile.outputs = [
@@ -115,62 +139,7 @@ in
               }
             ];
           }
-          {
-            profile = {
-              name = "docked_dual";
-              outputs = [
-                {
-                  criteria = "eDP-1";
-                  status = "disable";
-                }
-                {
-                  criteria = "Dell Inc. DELL U2719D 74RRT13";
-                  position = "0,0";
-                  mode = "2560x1440";
-                }
-                {
-                  criteria = "Dell Inc. DELL U2717D J0XYN99BA3TS";
-                  position = "2560,0";
-                  mode = "2560x1440";
-                }
-              ];
-              exec = [
-                "exec swaymsg workspace 10, move workspace to \"Dell Inc. DELL U2717D J0XYN99BA3TS\""
-                "exec swaymsg workspace 1, move workspace to \"Dell Inc. DELL U2719D 74RRT13\""
-                "exec swaymsg workspace 2, move workspace to \"Dell Inc. DELL U2719D 74RRT13\""
-              ];
-            };
-          }
-          {
-            profile = {
-              name = "docked_left";
-              outputs = [
-                {
-                  criteria = "eDP-1";
-                  position = "0,0";
-                  mode = "1920x1080";
-                }
-              ];
-            };
-          }
-          (createDualDockedIdenticalProfile {
-            left_criteria = "AOC 2460G5 F54GABA004914";
-            right_criteria = "Samsung Electric Company C27HG7x HTHJ700995";
-            width = 1920;
-            height = 1080;
-          })
-          (createDualDockedIdenticalProfile {
-            left_criteria = "Dell Inc. DELL U2719D 74RRT13";
-            right_criteria = "Dell Inc. DELL U2717D J0XYN99BA3TS";
-            width = 2560;
-            height = 1440;
-          })
-          (createDualDockedIdenticalProfile {
-            left_criteria = "Dell Inc. DELL U2717D J0XYN989CU9S";
-            right_criteria = "Dell Inc. DELL U2719D 7RRBC23";
-            width = 2560;
-            height = 1440;
-          })
+
         ]
         ++ (builtins.map createLeftDockedProfile [
           "Iiyama North America PL3288UH 1169612412790"
